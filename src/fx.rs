268feda345a99db7b02dfa65bc8667c1fecd66b5
Noam Raz <noamraz8@gmail.com>
2023-04-25 22:04:18 +0300

    Remove unused module

--- src/fx.rs
+++
@@ -1,7 +0,0 @@
-use core::hash::BuildHasherDefault;
-
-use hashbrown::{HashMap, HashSet};
-use rustc_hash::FxHasher;
-
-pub type FxHashMap<K, V> = HashMap<K, V, BuildHasherDefault<FxHasher>>;
-pub type FxHashSet<K> = HashSet<K, BuildHasherDefault<FxHasher>>;
--- src/lib.rs
+++ src/lib.rs
@@ -4,5 +4,3 @@ extern crate alloc;
 
 pub mod entities;
 pub mod graph;
-
-mod fx;

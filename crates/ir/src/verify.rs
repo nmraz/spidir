cbc25853c62e244372fc9d07352813efafa2df9b
Noam Raz <noamraz8@gmail.com>
2023-05-16 22:55:17 +0300

    Extract common integer verification logic

--- crates/ir/src/verify.rs
+++ crates/ir/src/verify.rs
@@ -69,69 +69,70 @@ fn verify_node_kind(graph: &ValGraph, node: Node, errors: &mut Vec<VerifierError
         NodeKind::Region => {}
         NodeKind::Phi => {}
         NodeKind::IConst(_) => {}
-        NodeKind::Iadd => {
-            let inputs = graph.node_inputs(node);
-            let outputs = graph.node_outputs(node);
-
-            if inputs.len() != 2 {
-                errors.push(VerifierError::BadInputCount { node, expected: 2 });
-                return;
-            }
-
-            if outputs.len() != 1 {
-                errors.push(VerifierError::BadOutputCount { node, expected: 1 });
-                return;
-            }
-
-            let result = outputs[0];
-            let in_a = inputs[0];
-            let in_b = inputs[1];
-
-            let result_kind = graph.value_kind(result);
-            if !result_kind.is_integer_value() {
-                errors.push(VerifierError::BadOutputKind {
-                    value: result,
-                    expected: vec![
-                        DepValueKind::Value(Type::I32),
-                        DepValueKind::Value(Type::I64),
-                    ],
-                });
-                return;
-            }
-
-            if graph.value_kind(in_a) != result_kind {
-                errors.push(VerifierError::BadInputKind {
-                    node,
-                    input: 0,
-                    expected: vec![result_kind],
-                });
-            }
-
-            if graph.value_kind(in_b) != result_kind {
-                errors.push(VerifierError::BadInputKind {
-                    node,
-                    input: 1,
-                    expected: vec![result_kind],
-                });
-            }
-        }
-        NodeKind::Isub => {}
-        NodeKind::And => {}
-        NodeKind::Or => {}
-        NodeKind::Xor => {}
+        NodeKind::Iadd => verify_int_binop(graph, node, errors),
+        NodeKind::Isub => verify_int_binop(graph, node, errors),
+        NodeKind::And => verify_int_binop(graph, node, errors),
+        NodeKind::Or => verify_int_binop(graph, node, errors),
+        NodeKind::Xor => verify_int_binop(graph, node, errors),
         NodeKind::Shl => {}
         NodeKind::Lshr => {}
         NodeKind::Ashr => {}
-        NodeKind::Smul => {}
-        NodeKind::Umul => {}
-        NodeKind::Sdiv => {}
-        NodeKind::Udiv => {}
+        NodeKind::Smul => verify_int_binop(graph, node, errors),
+        NodeKind::Umul => verify_int_binop(graph, node, errors),
+        NodeKind::Sdiv => verify_int_binop(graph, node, errors),
+        NodeKind::Udiv => verify_int_binop(graph, node, errors),
         NodeKind::Icmp(_) => {}
         NodeKind::FConst(_) => {}
         NodeKind::Load => {}
         NodeKind::Store => {}
         NodeKind::BrCond => {}
         NodeKind::Call(_) => {}
+    }
+}
+
+fn verify_int_binop(graph: &ValGraph, node: Node, errors: &mut Vec<VerifierError>) {
+    let inputs = graph.node_inputs(node);
+    let outputs = graph.node_outputs(node);
+
+    if inputs.len() != 2 {
+        errors.push(VerifierError::BadInputCount { node, expected: 2 });
+        return;
+    }
+
+    if outputs.len() != 1 {
+        errors.push(VerifierError::BadOutputCount { node, expected: 1 });
+        return;
+    }
+
+    let result = outputs[0];
+    let in_a = inputs[0];
+    let in_b = inputs[1];
+    let result_kind = graph.value_kind(result);
+    if !result_kind.is_integer_value() {
+        errors.push(VerifierError::BadOutputKind {
+            value: result,
+            expected: vec![
+                DepValueKind::Value(Type::I32),
+                DepValueKind::Value(Type::I64),
+            ],
+        });
+        return;
+    }
+
+    if graph.value_kind(in_a) != result_kind {
+        errors.push(VerifierError::BadInputKind {
+            node,
+            input: 0,
+            expected: vec![result_kind],
+        });
+    }
+
+    if graph.value_kind(in_b) != result_kind {
+        errors.push(VerifierError::BadInputKind {
+            node,
+            input: 1,
+            expected: vec![result_kind],
+        });
     }
 }
 

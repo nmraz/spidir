bb2199b67337a947b9e3d387c4dff12d6c86cd35
Noam Raz <noamraz8@gmail.com>
2023-07-01 20:10:46 +0300

    Create `NodeFactory` abstraction
    This will enable things like hash-consing later.

--- crates/ir/src/builder.rs
+++ crates/ir/src/builder.rs
@@ -5,6 +5,31 @@ use crate::{
     valgraph::{DepValue, Node, ValGraph},
 };
 
+pub trait NodeFactory {
+    fn create_node(
+        &mut self,
+        kind: NodeKind,
+        inputs: impl IntoIterator<Item = DepValue>,
+        output_kinds: impl IntoIterator<Item = DepValueKind>,
+    ) -> Node;
+    fn graph(&self) -> &ValGraph;
+}
+
+impl NodeFactory for ValGraph {
+    fn create_node(
+        &mut self,
+        kind: NodeKind,
+        inputs: impl IntoIterator<Item = DepValue>,
+        output_kinds: impl IntoIterator<Item = DepValueKind>,
+    ) -> Node {
+        self.create_node(kind, inputs, output_kinds)
+    }
+
+    fn graph(&self) -> &ValGraph {
+        self
+    }
+}
+
 pub struct BuiltEntry {
     pub node: Node,
     pub ctrl: DepValue,
@@ -36,8 +61,8 @@ pub struct BuiltEffectful {
     pub output: DepValue,
 }
 
-pub fn build_entry(graph: &mut ValGraph, types: &[Type]) -> BuiltEntry {
-    let entry = graph.create_node(
+pub fn build_entry(factory: &mut impl NodeFactory, types: &[Type]) -> BuiltEntry {
+    let entry = factory.create_node(
         NodeKind::Entry,
         [],
         iter::once(DepValueKind::Control).chain(types.iter().map(|&ty| DepValueKind::Value(ty))),
@@ -45,21 +70,21 @@ pub fn build_entry(graph: &mut ValGraph, types: &[Type]) -> BuiltEntry {
 
     BuiltEntry {
         node: entry,
-        ctrl: graph.node_outputs(entry)[0],
+        ctrl: factory.graph().node_outputs(entry)[0],
     }
 }
 
-pub fn build_return(graph: &mut ValGraph, ctrl: DepValue, value: Option<DepValue>) {
-    graph.create_node(NodeKind::Return, iter::once(ctrl).chain(value), []);
+pub fn build_return(factory: &mut impl NodeFactory, ctrl: DepValue, value: Option<DepValue>) {
+    factory.create_node(NodeKind::Return, iter::once(ctrl).chain(value), []);
 }
 
-pub fn build_region(graph: &mut ValGraph, incoming: &[DepValue]) -> BuiltRegion {
-    let region = graph.create_node(
+pub fn build_region(factory: &mut impl NodeFactory, incoming: &[DepValue]) -> BuiltRegion {
+    let region = factory.create_node(
         NodeKind::Region,
         incoming.iter().copied(),
         [DepValueKind::Control, DepValueKind::PhiSelector],
     );
-    let outputs = graph.node_outputs(region);
+    let outputs = factory.graph().node_outputs(region);
 
     BuiltRegion {
         node: region,
@@ -68,13 +93,13 @@ pub fn build_region(graph: &mut ValGraph, incoming: &[DepValue]) -> BuiltRegion 
     }
 }
 
-pub fn build_brcond(graph: &mut ValGraph, ctrl: DepValue, cond: DepValue) -> BuiltBrcond {
-    let brcond = graph.create_node(
+pub fn build_brcond(factory: &mut impl NodeFactory, ctrl: DepValue, cond: DepValue) -> BuiltBrcond {
+    let brcond = factory.create_node(
         NodeKind::BrCond,
         [ctrl, cond],
         [DepValueKind::Control, DepValueKind::Control],
     );
-    let outputs = graph.node_outputs(brcond);
+    let outputs = factory.graph().node_outputs(brcond);
     BuiltBrcond {
         true_ctrl: outputs[0],
         false_ctrl: outputs[1],
@@ -82,18 +107,18 @@ pub fn build_brcond(graph: &mut ValGraph, ctrl: DepValue, cond: DepValue) -> Bui
 }
 
 pub fn build_call(
-    graph: &mut ValGraph,
+    factory: &mut impl NodeFactory,
     ret_ty: Option<Type>,
     func: FunctionRef,
     ctrl: DepValue,
     args: &[DepValue],
 ) -> BuiltCall {
-    let call = graph.create_node(
+    let call = factory.create_node(
         NodeKind::Call(func),
         iter::once(ctrl).chain(args.iter().copied()),
         ret_ty.into_iter().map(DepValueKind::Value),
     );
-    let outputs = graph.node_outputs(call);
+    let outputs = factory.graph().node_outputs(call);
     BuiltCall {
         ctrl: outputs[0],
         retval: outputs.get(1).copied(),
@@ -101,125 +126,130 @@ pub fn build_call(
 }
 
 pub fn build_phi(
-    graph: &mut ValGraph,
+    factory: &mut impl NodeFactory,
     ty: Type,
     selector: DepValue,
     incoming_values: &[DepValue],
 ) -> BuiltPhi {
-    let phi = graph.create_node(
+    let phi = factory.create_node(
         NodeKind::Phi,
         iter::once(selector).chain(incoming_values.iter().copied()),
         [DepValueKind::Value(ty)],
     );
     BuiltPhi {
         node: phi,
-        output: graph.node_outputs(phi)[0],
+        output: factory.graph().node_outputs(phi)[0],
     }
 }
 
-pub fn build_iconst(graph: &mut ValGraph, ty: Type, value: u64) -> DepValue {
-    build_single_output_pure(graph, NodeKind::IConst(value), [], ty)
+pub fn build_iconst(factory: &mut impl NodeFactory, ty: Type, value: u64) -> DepValue {
+    build_single_output_pure(factory, NodeKind::IConst(value), [], ty)
 }
-pub fn build_fconst(graph: &mut ValGraph, ty: Type, value: f64) -> DepValue {
-    build_single_output_pure(graph, NodeKind::FConst(value), [], ty)
+pub fn build_fconst(factory: &mut impl NodeFactory, ty: Type, value: f64) -> DepValue {
+    build_single_output_pure(factory, NodeKind::FConst(value), [], ty)
 }
 
-pub fn build_iadd(graph: &mut ValGraph, ty: Type, a: DepValue, b: DepValue) -> DepValue {
-    build_single_output_pure(graph, NodeKind::Iadd, [a, b], ty)
+pub fn build_iadd(factory: &mut impl NodeFactory, ty: Type, a: DepValue, b: DepValue) -> DepValue {
+    build_single_output_pure(factory, NodeKind::Iadd, [a, b], ty)
 }
-pub fn build_isub(graph: &mut ValGraph, ty: Type, a: DepValue, b: DepValue) -> DepValue {
-    build_single_output_pure(graph, NodeKind::Isub, [a, b], ty)
+pub fn build_isub(factory: &mut impl NodeFactory, ty: Type, a: DepValue, b: DepValue) -> DepValue {
+    build_single_output_pure(factory, NodeKind::Isub, [a, b], ty)
 }
-pub fn build_and(graph: &mut ValGraph, ty: Type, a: DepValue, b: DepValue) -> DepValue {
-    build_single_output_pure(graph, NodeKind::And, [a, b], ty)
+pub fn build_and(factory: &mut impl NodeFactory, ty: Type, a: DepValue, b: DepValue) -> DepValue {
+    build_single_output_pure(factory, NodeKind::And, [a, b], ty)
 }
-pub fn build_or(graph: &mut ValGraph, ty: Type, a: DepValue, b: DepValue) -> DepValue {
-    build_single_output_pure(graph, NodeKind::Or, [a, b], ty)
+pub fn build_or(factory: &mut impl NodeFactory, ty: Type, a: DepValue, b: DepValue) -> DepValue {
+    build_single_output_pure(factory, NodeKind::Or, [a, b], ty)
 }
-pub fn build_xor(graph: &mut ValGraph, ty: Type, a: DepValue, b: DepValue) -> DepValue {
-    build_single_output_pure(graph, NodeKind::Xor, [a, b], ty)
+pub fn build_xor(factory: &mut impl NodeFactory, ty: Type, a: DepValue, b: DepValue) -> DepValue {
+    build_single_output_pure(factory, NodeKind::Xor, [a, b], ty)
 }
-pub fn build_shl(graph: &mut ValGraph, ty: Type, a: DepValue, b: DepValue) -> DepValue {
-    build_single_output_pure(graph, NodeKind::Shl, [a, b], ty)
+pub fn build_shl(factory: &mut impl NodeFactory, ty: Type, a: DepValue, b: DepValue) -> DepValue {
+    build_single_output_pure(factory, NodeKind::Shl, [a, b], ty)
 }
-pub fn build_lshr(graph: &mut ValGraph, ty: Type, a: DepValue, b: DepValue) -> DepValue {
-    build_single_output_pure(graph, NodeKind::Lshr, [a, b], ty)
+pub fn build_lshr(factory: &mut impl NodeFactory, ty: Type, a: DepValue, b: DepValue) -> DepValue {
+    build_single_output_pure(factory, NodeKind::Lshr, [a, b], ty)
 }
-pub fn build_ashr(graph: &mut ValGraph, ty: Type, a: DepValue, b: DepValue) -> DepValue {
-    build_single_output_pure(graph, NodeKind::Ashr, [a, b], ty)
+pub fn build_ashr(factory: &mut impl NodeFactory, ty: Type, a: DepValue, b: DepValue) -> DepValue {
+    build_single_output_pure(factory, NodeKind::Ashr, [a, b], ty)
 }
-pub fn build_imul(graph: &mut ValGraph, ty: Type, a: DepValue, b: DepValue) -> DepValue {
-    build_single_output_pure(graph, NodeKind::Imul, [a, b], ty)
+pub fn build_imul(factory: &mut impl NodeFactory, ty: Type, a: DepValue, b: DepValue) -> DepValue {
+    build_single_output_pure(factory, NodeKind::Imul, [a, b], ty)
 }
 pub fn build_icmp(
-    graph: &mut ValGraph,
+    factory: &mut impl NodeFactory,
     kind: IcmpKind,
     output_ty: Type,
     a: DepValue,
     b: DepValue,
 ) -> DepValue {
-    build_single_output_pure(graph, NodeKind::Icmp(kind), [a, b], output_ty)
+    build_single_output_pure(factory, NodeKind::Icmp(kind), [a, b], output_ty)
 }
 
 pub fn build_sdiv(
-    graph: &mut ValGraph,
+    factory: &mut impl NodeFactory,
     ty: Type,
     ctrl: DepValue,
     a: DepValue,
     b: DepValue,
 ) -> BuiltEffectful {
-    build_int_div(graph, NodeKind::Sdiv, ctrl, a, b, ty)
+    build_int_div(factory, NodeKind::Sdiv, ctrl, a, b, ty)
 }
 pub fn build_udiv(
-    graph: &mut ValGraph,
+    factory: &mut impl NodeFactory,
     ty: Type,
     ctrl: DepValue,
     a: DepValue,
     b: DepValue,
 ) -> BuiltEffectful {
-    build_int_div(graph, NodeKind::Udiv, ctrl, a, b, ty)
+    build_int_div(factory, NodeKind::Udiv, ctrl, a, b, ty)
 }
 
-pub fn build_ptroff(graph: &mut ValGraph, a: DepValue, b: DepValue) -> DepValue {
-    build_single_output_pure(graph, NodeKind::PtrOff, [a, b], Type::Ptr)
+pub fn build_ptroff(factory: &mut impl NodeFactory, a: DepValue, b: DepValue) -> DepValue {
+    build_single_output_pure(factory, NodeKind::PtrOff, [a, b], Type::Ptr)
 }
 
-pub fn build_load(graph: &mut ValGraph, ty: Type, ctrl: DepValue, ptr: DepValue) -> BuiltEffectful {
-    let load = graph.create_node(
+pub fn build_load(
+    factory: &mut impl NodeFactory,
+    ty: Type,
+    ctrl: DepValue,
+    ptr: DepValue,
+) -> BuiltEffectful {
+    let load = factory.create_node(
         NodeKind::Load,
         [ctrl, ptr],
         [DepValueKind::Control, DepValueKind::Value(ty)],
     );
-    let outputs = graph.node_outputs(load);
+    let outputs = factory.graph().node_outputs(load);
     BuiltEffectful {
         ctrl: outputs[0],
         output: outputs[1],
     }
 }
 pub fn build_store(
-    graph: &mut ValGraph,
+    factory: &mut impl NodeFactory,
     ctrl: DepValue,
     data: DepValue,
     ptr: DepValue,
 ) -> DepValue {
-    let store = graph.create_node(NodeKind::Store, [ctrl, data, ptr], [DepValueKind::Control]);
-    graph.node_outputs(store)[0]
+    let store = factory.create_node(NodeKind::Store, [ctrl, data, ptr], [DepValueKind::Control]);
+    factory.graph().node_outputs(store)[0]
 }
 
 fn build_int_div(
-    graph: &mut ValGraph,
+    factory: &mut impl NodeFactory,
     kind: NodeKind,
     ctrl: DepValue,
     a: DepValue,
     b: DepValue,
     output_ty: Type,
 ) -> BuiltEffectful {
-    let node = graph.create_node(
+    let node = factory.create_node(
         kind,
         [ctrl, a, b],
         [DepValueKind::Control, DepValueKind::Value(output_ty)],
     );
-    let outputs = graph.node_outputs(node);
+    let outputs = factory.graph().node_outputs(node);
     BuiltEffectful {
         ctrl: outputs[0],
         output: outputs[1],
@@ -227,11 +257,11 @@ fn build_int_div(
 }
 
 fn build_single_output_pure(
-    graph: &mut ValGraph,
+    factory: &mut impl NodeFactory,
     kind: NodeKind,
     inputs: impl IntoIterator<Item = DepValue>,
     output_ty: Type,
 ) -> DepValue {
-    let node = graph.create_node(kind, inputs, [DepValueKind::Value(output_ty)]);
-    graph.node_outputs(node)[0]
+    let node = factory.create_node(kind, inputs, [DepValueKind::Value(output_ty)]);
+    factory.graph().node_outputs(node)[0]
 }

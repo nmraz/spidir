31eabb4b990e00fd423dcc744c3b0f0edd04ee67
Noam Raz <noamraz8@gmail.com>
2023-07-08 21:52:00 +0300

    Add test for phi handles with C API

--- c-api-tests/cases/phi.c
+++ c-api-tests/cases/phi.c
@@ -22,6 +22,30 @@ void select_phi_prebuilt_callback(spidir_builder_handle_t builder, void* ctx) {
     spidir_builder_build_return(builder, &val);
 }
 
+void select_phi_handle_callback(spidir_builder_handle_t builder, void* ctx) {
+    (void) ctx;
+    spidir_block_t entry = spidir_builder_create_block(builder);
+    spidir_builder_set_entry_block(builder, entry);
+    spidir_builder_set_block(builder, entry);
+
+    spidir_value_t selector = spidir_builder_build_param_ref(builder, 0);
+    spidir_value_t a = spidir_builder_build_param_ref(builder, 1);
+    spidir_value_t b = spidir_builder_build_param_ref(builder, 2);
+
+    spidir_block_t exit_block = spidir_builder_create_block(builder);
+    spidir_builder_build_brcond(builder, selector, exit_block, exit_block);
+
+    spidir_builder_set_block(builder, exit_block);
+    spidir_phi_t phi;
+    spidir_value_t val =
+        spidir_builder_build_phi(builder, SPIDIR_TYPE_I32, 0, NULL, &phi);
+
+    spidir_builder_add_phi_input(builder, phi, a);
+    spidir_builder_add_phi_input(builder, phi, b);
+
+    spidir_builder_build_return(builder, &val);
+}
+
 int main(void) {
     spidir_module_handle_t module = spidir_module_create();
 
@@ -33,6 +57,11 @@ int main(void) {
         module, "select_phi_prebuilt", &ret_type, 3, param_types);
     spidir_module_build_function(module, phi_prebuilt_func,
                                  select_phi_prebuilt_callback, NULL);
+
+    spidir_function_t phi_handle_func = spidir_module_create_function(
+        module, "select_phi_handle", &ret_type, 3, param_types);
+    spidir_module_build_function(module, phi_handle_func,
+                                 select_phi_handle_callback, NULL);
 
     dump_module_to_stdout(module);
 

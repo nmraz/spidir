5e128cca75b4c679a2dbdafd5c0d83152fec1309
Noam Raz <noamraz8@gmail.com>
2023-07-08 19:16:49 +0300

    Create compilation database in C test script

--- c-api-tests/run.sh
+++ c-api-tests/run.sh
@@ -2,7 +2,10 @@
 
 cd $(dirname $(readlink -f "$0"))
 
+export CMAKE_EXPORT_COMPILE_COMMANDS=ON
+export CTEST_OUTPUT_ON_FAILURE=1
+
 mkdir -p build &&
     cmake -S . -B build &&
     cmake --build build &&
-    CTEST_OUTPUT_ON_FAILURE=1 cmake --build build --target test
+    cmake --build build --target test

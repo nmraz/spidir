36063eb1a0d4cef0e17c7abad237c4ac946c7396
Noam Raz <noamraz8@gmail.com>
2023-08-22 22:51:38 +0300

    Begin implementing line extraction for filetests

--- crates/filetests/src/lib.rs
+++ crates/filetests/src/lib.rs
@@ -1,3 +1,4 @@
+use core::iter;
 use std::{fmt::Write, sync::OnceLock};
 
 use cranelift_entity::SecondaryMap;
@@ -13,7 +14,7 @@ use regex::Regex;
 
 pub trait TestProducer {
     fn output_for(&self, module: &Module) -> String;
-    fn update(&self, module: &Module, input_str: &str, output_str: &str);
+    fn update(&self, module: &Module, input_lines: &mut Vec<String>, output_str: &str);
 }
 
 pub struct VerifyTest;
@@ -64,9 +65,16 @@ impl TestProducer for VerifyTest {
         output
     }
 
-    fn update(&self, _module: &Module, _input_str: &str, _output_str: &str) {
+    fn update(&self, _module: &Module, input_lines: &mut Vec<String>, _output_str: &str) {
+        eprintln!("lines: {}", input_lines.join("\n"));
         todo!()
     }
+}
+
+fn display_node(module: &Module, graph: &ValGraph, node: Node) -> String {
+    let mut s = String::new();
+    write_node(&mut s, module, graph, node).unwrap();
+    s
 }
 
 pub fn select_test_producer(input: &str) -> Box<dyn TestProducer> {
@@ -85,38 +93,238 @@ pub fn select_test_producer(input: &str) -> Box<dyn TestProducer> {
     }
 }
 
-pub fn run_test(producer: &dyn TestProducer, input: &str) {
-    let checker = build_checker(input);
+pub fn run_test(producer: &dyn TestProducer, input: &str, update_if_failed: bool) {
+    let (checker, mut lines) = build_checker_and_lines(input);
     let module = parse_module(input).expect("failed to parse module");
     let output = producer.output_for(&module);
     let (ok, explanation) = checker
         .explain(&output, filecheck::NO_VARIABLES)
         .expect("bad filecheck directive");
     if !ok {
-        eprintln!("{}", explanation);
-        panic!("checks failed");
+        if update_if_failed {
+            producer.update(&module, &mut lines, &output);
+        } else {
+            eprintln!("{}", explanation);
+            panic!("checks failed");
+        }
     }
-}
-
-fn display_node(module: &Module, graph: &ValGraph, node: Node) -> String {
-    let mut s = String::new();
-    write_node(&mut s, module, graph, node).unwrap();
-    s
-}
-
-fn build_checker(input: &str) -> Checker {
-    let mut builder = CheckerBuilder::new();
-    for comment in comments(input) {
-        builder.directive(comment).expect("invalid directive");
-    }
-    let checker = builder.finish();
-    assert!(!checker.is_empty(), "no filecheck directives in input file");
-    checker
 }
 
 fn comments(input: &str) -> impl Iterator<Item = &str> {
     input.lines().filter_map(|line| {
-        line.find('#')
-            .map(|comment_start| line[comment_start + 1..].trim())
+        find_comment_start(line).map(|comment_start| line[comment_start + 1..].trim())
     })
+}
+
+fn build_checker_and_lines(input: &str) -> (Checker, Vec<String>) {
+    let mut other_lines = Vec::new();
+    let mut builder = CheckerBuilder::new();
+    let mut empty_line_run = 0;
+    let mut post_directive_only = false;
+
+    for line in input.lines() {
+        if line.trim().is_empty() {
+            if !post_directive_only {
+                // Record any empty lines that don't follow a directive-only line so we can add them
+                // back in later.
+                empty_line_run += 1;
+            }
+            continue;
+        }
+
+        if let Some(comment_start) = find_comment_start(line) {
+            let comment = line[comment_start + 1..].trim();
+
+            let was_directive = builder.directive(comment).expect("invalid directive");
+
+            let pre_line = if was_directive {
+                line[..comment_start].trim()
+            } else {
+                line
+            };
+
+            if pre_line.is_empty() {
+                // This was a directive-only line, so delete any empty lines that came before it.
+                post_directive_only = was_directive;
+            } else {
+                other_lines.extend(iter::repeat(String::new()).take(empty_line_run));
+                other_lines.push(pre_line.to_owned());
+                post_directive_only = false;
+            }
+        } else {
+            other_lines.extend(iter::repeat(String::new()).take(empty_line_run));
+            other_lines.push(line.to_owned());
+            post_directive_only = false;
+        }
+
+        empty_line_run = 0;
+    }
+
+    let checker = builder.finish();
+    assert!(!checker.is_empty(), "no filecheck directives in input file");
+
+    (checker, other_lines)
+}
+
+fn find_comment_start(line: &str) -> Option<usize> {
+    line.find('#')
+}
+
+#[cfg(test)]
+mod tests {
+    use expect_test::{expect, Expect};
+
+    use super::*;
+
+    fn check_lines(input: &str, expected: Expect) {
+        let (_, lines) = build_checker_and_lines(input);
+        let lines = lines.join("\n");
+        expected.assert_eq(&lines);
+    }
+
+    #[test]
+    fn lines_empty() {
+        check_lines(
+            "
+            # check: a
+
+
+        ",
+            expect![[""]],
+        );
+    }
+
+    #[test]
+    fn lines_simple() {
+        check_lines(
+            r"
+# run: verify
+
+# regex: val=%\d+
+
+func @test(ptr, i32) {
+    # check: function `test`:
+    # nextln: `$val:ctrl, $val:i32, $val:ptr = entry`: bad value kind for output 1, expected one of `ptr`, got `i32`
+    %0:ctrl, %2:i32, %1:ptr = entry
+
+    # fsjlkdf
+    return %0
+
+}
+            ",
+            expect![[r#"
+
+                            # run: verify
+                            func @test(ptr, i32) {
+                                %0:ctrl, %2:i32, %1:ptr = entry
+
+                                # fsjlkdf
+                                return %0
+
+                            }"#]],
+        );
+    }
+
+    #[test]
+    fn lines_interleaved_directives() {
+        check_lines(
+            r"
+# run: verify
+
+# regex: val=%\d+
+
+func @test(ptr, i32) {
+    # check: function `test`:
+    # a
+    # nextln: `$val:ctrl, $val:i32, $val:ptr = entry`: bad value kind for output 1, expected one of `ptr`, got `i32`
+    # b
+    %0:ctrl, %2:i32, %1:ptr = entry
+
+    # fsjlkdf
+    return %0
+}
+            ",
+            expect![[r#"
+
+                            # run: verify
+                            func @test(ptr, i32) {
+                                # a
+                                # b
+                                %0:ctrl, %2:i32, %1:ptr = entry
+
+                                # fsjlkdf
+                                return %0
+                            }"#]],
+        );
+    }
+
+    #[test]
+    fn lines_interleaved_whitespace() {
+        check_lines(
+            r"
+# run: verify
+
+# regex: val=%\d+
+
+func @test(ptr, i32) {
+    # check: function `test`:
+
+
+    # nextln: `$val:ctrl, $val:i32, $val:ptr = entry`: bad value kind for output 1, expected one of `ptr`, got `i32`
+
+
+    # nextln: sdjlkf
+    %0:ctrl, %2:i32, %1:ptr = entry
+
+    # fsjlkdf
+    return %0
+
+}
+        ",
+            expect![[r#"
+
+                            # run: verify
+                            func @test(ptr, i32) {
+                                %0:ctrl, %2:i32, %1:ptr = entry
+
+                                # fsjlkdf
+                                return %0
+
+                            }"#]],
+        )
+    }
+
+    #[test]
+    fn lines_leading_trailing_whitespace() {
+        check_lines(
+            r"
+# run: verify
+
+# regex: val=%\d+
+
+func @test(ptr, i32) {
+
+
+    # nextln: `$val:ctrl, $val:i32, $val:ptr = entry`: bad value kind for output 1, expected one of `ptr`, got `i32`
+
+
+    %0:ctrl, %2:i32, %1:ptr = entry
+
+    # fsjlkdf
+    return %0
+
+}
+        ",
+            expect![[r#"
+
+                            # run: verify
+                            func @test(ptr, i32) {
+                                %0:ctrl, %2:i32, %1:ptr = entry
+
+                                # fsjlkdf
+                                return %0
+
+                            }"#]],
+        )
+    }
 }
--- crates/filetests/src/main.rs
+++ crates/filetests/src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs, path::Path};
+use std::{env, fs, path::Path};
 
 use filetests::{run_test, select_test_producer};
 
@@ -6,5 +6,5 @@ fn main() {
     let path = Path::new(env!("CARGO_MANIFEST_DIR")).join("filetests/verify.spdr");
     let contents = fs::read_to_string(path).expect("failed to read file");
     let producer = select_test_producer(&contents);
-    run_test(&*producer, &contents);
+    run_test(&*producer, &contents, env::var("UPDATE_EXPECT").is_ok());
 }

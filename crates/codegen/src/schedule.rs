b891328a6f896dcae8aa60beb4401a810382e44c
Noam Raz <noamraz8@gmail.com>
2023-09-17 19:00:52 +0300

    Refactor partial scheduling implementation

--- crates/codegen/src/schedule.rs
+++ crates/codegen/src/schedule.rs
@@ -19,35 +19,11 @@ pub struct Schedule {
 
 impl Schedule {
     pub fn compute(graph: &ValGraph, domtree: &DomTree, loop_forest: &LoopForest) -> Self {
-        let domtree_preorder: Vec<_> = domtree.preorder().collect();
-        let live_node_info = LiveNodeInfo::compute(graph, domtree.get_cfg_node(domtree.root()));
-        let mut pinned_nodes = ByNodeSchedule::new();
+        let ctx = ScheduleCtx::prepare(graph, domtree, loop_forest);
 
-        for &domtree_node in &domtree_preorder {
-            let cfg_node = domtree.get_cfg_node(domtree_node);
-            pinned_nodes[cfg_node] = domtree_node.into();
-            for phi in get_attached_phis(graph, cfg_node) {
-                pinned_nodes[phi] = domtree_node.into();
-            }
-        }
+        let early_schedule = schedule_early(&ctx);
 
-        let early_schedule = schedule_early(
-            graph,
-            live_node_info.live_nodes(),
-            domtree,
-            &domtree_preorder,
-            &pinned_nodes,
-        );
-
-        schedule_late(
-            graph,
-            domtree,
-            loop_forest,
-            &domtree_preorder,
-            &live_node_info,
-            &pinned_nodes,
-            &early_schedule,
-        );
+        schedule_late(&ctx, &early_schedule);
 
         todo!()
     }
@@ -89,6 +65,8 @@ impl<'a> Graph for UnpinnedDataPreds<'a> {
     }
 }
 
+type DataPredPostOrder<'a> = PostOrderContext<UnpinnedDataPreds<'a>>;
+
 struct UnpinnedDataSuccs<'a> {
     graph: &'a ValGraph,
     live_nodes: &'a EntitySet<Node>,
@@ -121,42 +99,60 @@ impl<'a> Graph for UnpinnedDataSuccs<'a> {
     }
 }
 
-fn schedule_early(
-    graph: &ValGraph,
-    live_nodes: &EntitySet<Node>,
-    domtree: &DomTree,
-    domtree_preorder: &[DomTreeNode],
-    pinned_nodes: &ByNodeSchedule,
-) -> ByNodeSchedule {
-    let mut schedule = pinned_nodes.clone();
+struct ScheduleCtx<'a> {
+    graph: &'a ValGraph,
+    domtree: &'a DomTree,
+    loop_forest: &'a LoopForest,
+    domtree_preorder: Vec<DomTreeNode>,
+    live_node_info: LiveNodeInfo,
+    pinned_nodes: ByNodeSchedule,
+}
+
+impl<'a> ScheduleCtx<'a> {
+    fn prepare(graph: &'a ValGraph, domtree: &'a DomTree, loop_forest: &'a LoopForest) -> Self {
+        let domtree_preorder: Vec<_> = domtree.preorder().collect();
+        let live_node_info = LiveNodeInfo::compute(graph, domtree.get_cfg_node(domtree.root()));
+        let mut pinned_nodes = ByNodeSchedule::new();
+
+        for &domtree_node in &domtree_preorder {
+            let cfg_node = domtree.get_cfg_node(domtree_node);
+            pinned_nodes[cfg_node] = domtree_node.into();
+            for phi in get_attached_phis(graph, cfg_node) {
+                pinned_nodes[phi] = domtree_node.into();
+            }
+        }
+
+        Self {
+            graph,
+            domtree,
+            loop_forest,
+            domtree_preorder,
+            live_node_info,
+            pinned_nodes,
+        }
+    }
+}
+
+fn schedule_early(ctx: &ScheduleCtx<'_>) -> ByNodeSchedule {
+    let mut schedule = ctx.pinned_nodes.clone();
     let mut visited = EntitySet::new();
 
     let mut input_postorder =
-        PostOrderContext::new(UnpinnedDataPreds::new(graph, pinned_nodes), []);
+        PostOrderContext::new(UnpinnedDataPreds::new(ctx.graph, &ctx.pinned_nodes), []);
 
-    for &domtree_node in domtree_preorder {
-        let cfg_node = domtree.get_cfg_node(domtree_node);
+    for &domtree_node in &ctx.domtree_preorder {
+        let cfg_node = ctx.domtree.get_cfg_node(domtree_node);
         schedule_inputs_early(
-            graph,
-            domtree,
-            cfg_node,
+            ctx,
             &mut input_postorder,
             &mut visited,
-            pinned_nodes,
             &mut schedule,
+            cfg_node,
         );
 
-        for phi in get_attached_phis(graph, cfg_node) {
-            if live_nodes.contains(phi) {
-                schedule_inputs_early(
-                    graph,
-                    domtree,
-                    phi,
-                    &mut input_postorder,
-                    &mut visited,
-                    pinned_nodes,
-                    &mut schedule,
-                );
+        for phi in get_attached_phis(ctx.graph, cfg_node) {
+            if ctx.live_node_info.live_nodes().contains(phi) {
+                schedule_inputs_early(ctx, &mut input_postorder, &mut visited, &mut schedule, phi);
             }
         }
     }
@@ -165,141 +161,104 @@ fn schedule_early(
 }
 
 fn schedule_inputs_early(
-    graph: &ValGraph,
-    domtree: &DomTree,
-    node: Node,
-    input_postorder: &mut PostOrderContext<UnpinnedDataPreds<'_>>,
+    ctx: &ScheduleCtx<'_>,
+    input_postorder: &mut DataPredPostOrder<'_>,
     visited: &mut EntitySet<Node>,
-    pinned_nodes: &ByNodeSchedule,
     schedule: &mut ByNodeSchedule,
+    node: Node,
 ) {
     input_postorder.reset([node]);
     while let Some(data_pred) = input_postorder.next(visited) {
-        // We want to skip pinned nodes, which will have already been scheduled at this
-        // point.
-        if pinned_nodes[data_pred].is_some() {
-            continue;
-        }
+        debug_assert!(ctx.pinned_nodes[data_pred].is_none());
         debug_assert!(schedule[data_pred].is_none());
-        schedule[data_pred] = early_schedule_location(graph, domtree, schedule, data_pred).into();
+        schedule[data_pred] = early_schedule_location(ctx, schedule, data_pred).into();
     }
 }
 
 fn early_schedule_location(
-    graph: &ValGraph,
-    domtree: &DomTree,
+    ctx: &ScheduleCtx<'_>,
     schedule: &ByNodeSchedule,
     node: Node,
 ) -> DomTreeNode {
-    data_preds(graph, node)
+    data_preds(ctx.graph, node)
         .map(|pred| schedule[pred].expect("data flow cycle or dead input"))
         .reduce(|acc, cur| {
             // Find the deepest input schedule location in the dominator tree, assuming they are
             // totally ordered by dominance.
-            if domtree.dominates(acc, cur) {
+            if ctx.domtree.dominates(acc, cur) {
                 cur
             } else {
                 acc
             }
         })
-        .unwrap_or(domtree.root())
+        .unwrap_or(ctx.domtree.root())
 }
 
-fn schedule_late(
-    graph: &ValGraph,
-    domtree: &DomTree,
-    loop_forest: &LoopForest,
-    domtree_preorder: &[DomTreeNode],
-    live_node_info: &LiveNodeInfo,
-    pinned_nodes: &ByNodeSchedule,
-    early_schedule: &ByNodeSchedule,
-) -> ByNodeSchedule {
-    let depth_map = get_cfg_depth_map(domtree, loop_forest, domtree_preorder);
+fn schedule_late(ctx: &ScheduleCtx<'_>, early_schedule: &ByNodeSchedule) -> ByNodeSchedule {
+    let depth_map = get_cfg_depth_map(ctx);
     let mut late_visited_nodes = EntitySet::new();
-    let mut late_schedule = pinned_nodes.clone();
+    let mut late_schedule = ctx.pinned_nodes.clone();
     let mut output_postorder = PostOrderContext::new(
-        UnpinnedDataSuccs::new(graph, live_node_info.live_nodes(), pinned_nodes),
+        UnpinnedDataSuccs::new(
+            ctx.graph,
+            ctx.live_node_info.live_nodes(),
+            &ctx.pinned_nodes,
+        ),
         [],
     );
 
-    for &domtree_node in domtree_preorder {
-        let cfg_node = domtree.get_cfg_node(domtree_node);
+    for &domtree_node in &ctx.domtree_preorder {
+        let cfg_node = ctx.domtree.get_cfg_node(domtree_node);
         output_postorder.reset([cfg_node]);
         while let Some(data_succ) = output_postorder.next(&mut late_visited_nodes) {
             schedule_node_late(
-                graph,
-                domtree,
-                data_succ,
+                ctx,
                 &depth_map,
-                live_node_info,
-                pinned_nodes,
                 early_schedule,
                 &mut late_schedule,
+                data_succ,
             );
         }
 
-        for phi in get_attached_phis(graph, cfg_node) {
-            if live_node_info.live_nodes().contains(phi) {
+        for phi in get_attached_phis(ctx.graph, cfg_node) {
+            if ctx.live_node_info.live_nodes().contains(phi) {
                 output_postorder.reset([phi]);
                 while let Some(data_succ) = output_postorder.next(&mut late_visited_nodes) {
                     schedule_node_late(
-                        graph,
-                        domtree,
-                        data_succ,
+                        ctx,
                         &depth_map,
-                        live_node_info,
-                        pinned_nodes,
                         early_schedule,
                         &mut late_schedule,
+                        data_succ,
                     );
                 }
             }
         }
     }
 
-    for &root in live_node_info.roots() {
+    for &root in ctx.live_node_info.roots() {
         if late_schedule[root].is_none() {
-            schedule_node_late(
-                graph,
-                domtree,
-                root,
-                &depth_map,
-                live_node_info,
-                pinned_nodes,
-                early_schedule,
-                &mut late_schedule,
-            );
+            schedule_node_late(ctx, &depth_map, early_schedule, &mut late_schedule, root);
         }
     }
 
     late_schedule
 }
 
-#[allow(clippy::too_many_arguments)]
 fn schedule_node_late(
-    graph: &ValGraph,
-    domtree: &DomTree,
-    node: Node,
+    ctx: &ScheduleCtx<'_>,
     depth_map: &CfgDepthMap,
-    live_node_info: &LiveNodeInfo,
-    pinned_nodes: &ByNodeSchedule,
     early_schedule: &ByNodeSchedule,
     late_schedule: &mut ByNodeSchedule,
+    node: Node,
 ) {
-    debug_assert!(pinned_nodes[node].is_none());
+    debug_assert!(ctx.pinned_nodes[node].is_none());
     debug_assert!(late_schedule[node].is_none());
 
     let early_loc = early_schedule[node].unwrap();
-    let mut late_loc = late_schedule_location(
-        graph,
-        domtree,
-        depth_map,
-        live_node_info.live_nodes(),
-        late_schedule,
-        node,
-    );
+    let mut late_loc = late_schedule_location(ctx, depth_map, late_schedule, node);
 
-    debug_assert!(domtree.dominates(early_loc, late_loc));
+    debug_assert!(ctx.domtree.dominates(early_loc, late_loc));
 
     // Select a node between `early_loc` and `late_loc` on the dominator
     // tree that has minimal loop depth, favoring deeper nodes (those closer to `late_loc`).
@@ -308,38 +267,36 @@ fn schedule_node_late(
         if depth_map[late_loc].loop_depth < depth_map[best_loc].loop_depth {
             best_loc = late_loc;
         }
-        late_loc = domtree.idom(late_loc).unwrap();
+        late_loc = ctx.domtree.idom(late_loc).unwrap();
     }
 
     late_schedule[node] = best_loc.into();
 }
 
 fn late_schedule_location(
-    graph: &ValGraph,
-    domtree: &DomTree,
+    ctx: &ScheduleCtx<'_>,
     depth_map: &CfgDepthMap,
-    live_nodes: &EntitySet<Node>,
     schedule: &ByNodeSchedule,
     node: Node,
 ) -> DomTreeNode {
-    data_succs(graph, live_nodes, node)
+    data_succs(ctx.graph, ctx.live_node_info.live_nodes(), node)
         .filter_map(|(succ, input_idx)| {
-            if graph.node_kind(node) != &NodeKind::Phi {
+            if ctx.graph.node_kind(node) != &NodeKind::Phi {
                 // Plain data nodes using the value should always have a schedule if they are live.
                 Some(schedule[succ].expect("data flow cycle"))
             } else {
-                let region = graph.value_def(graph.node_inputs(node)[0]).0;
+                let region = ctx.graph.value_def(ctx.graph.node_inputs(node)[0]).0;
 
                 // Note: the corresponding region input might be dead even when the phi is live;
                 // this just means that this instance doesn't count as a use and can be skipped.
-                domtree.get_tree_node(
-                    graph
-                        .value_def(graph.node_inputs(region)[input_idx as usize])
+                ctx.domtree.get_tree_node(
+                    ctx.graph
+                        .value_def(ctx.graph.node_inputs(region)[input_idx as usize])
                         .0,
                 )
             }
         })
-        .reduce(|acc, cur| domtree_lca(domtree, depth_map, acc, cur))
+        .reduce(|acc, cur| domtree_lca(ctx.domtree, depth_map, acc, cur))
         .expect("live non-pinned node has no data uses")
 }
 
@@ -351,20 +308,20 @@ struct CfgDepthData {
 
 type CfgDepthMap = SecondaryMap<DomTreeNode, CfgDepthData>;
 
-fn get_cfg_depth_map(
-    domtree: &DomTree,
-    loop_forest: &LoopForest,
-    domtree_preorder: &[DomTreeNode],
-) -> CfgDepthMap {
+fn get_cfg_depth_map(ctx: &ScheduleCtx<'_>) -> CfgDepthMap {
     let mut depth_map = CfgDepthMap::new();
 
-    for &domtree_node in domtree_preorder {
+    for &domtree_node in &ctx.domtree_preorder {
         // TODO: `loop_depth` is linear in the loop depth, which is probably okay in practice, but
         // can cause worst-case quadratic behavior.
-        let loop_depth = loop_forest
+        let loop_depth = ctx
+            .loop_forest
             .containing_loop(domtree_node)
-            .map_or(0, |containing_loop| loop_forest.loop_depth(containing_loop));
-        let domtree_depth = domtree
+            .map_or(0, |containing_loop| {
+                ctx.loop_forest.loop_depth(containing_loop)
+            });
+        let domtree_depth = ctx
+            .domtree
             .idom(domtree_node)
             .map_or(0, |idom| depth_map[idom].domtree_depth + 1);
 

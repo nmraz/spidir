37249a9c27b7b50a15901f1708e9ad1f09cbe16f
Noam Raz <noamraz8@gmail.com>
2023-12-08 22:57:16 +0200

    Implement basic isel phi lowering

--- crates/codegen/src/isel.rs
+++ crates/codegen/src/isel.rs
@@ -8,6 +8,7 @@ use ir::{
     valgraph::{DepValue, Node, ValGraph},
     valwalk::{dataflow_inputs, dataflow_outputs, dataflow_preds},
 };
+use smallvec::SmallVec;
 
 use crate::{
     cfg::{Block, CfgContext},
@@ -131,24 +132,84 @@ pub fn select_instrs<B: Backend>(
 ) -> Result<Lir<B::Instr>, IselFailed> {
     // TODO: Classify side effects.
     // TODO: Collect stack slots and prepare dedicated stack slot mapping.
-    // TODO: Gather phis and convert to block params.
 
     let mut value_reg_map = ValueRegMap::default();
     let mut reg_node_map = RegNodeMap::default();
     let mut node_use_counts = NodeUseCountMap::new();
 
+    let mut lir_builder = LirBuilder::new(&cfg_ctx.block_order);
+
     // Count original node uses before we start selection.
     for &block in &cfg_ctx.block_order {
+        for &phi in schedule.block_phis(block) {
+            // Values flowing into block parameters are always live.
+            // TODO: Except for values from dead control inputs?
+            for pred in dataflow_preds(valgraph, phi) {
+                node_use_counts[pred] += 1;
+            }
+
+            // Create a fresh register for every incoming block parameter (phi output).
+            let output = valgraph.node_outputs(phi)[0];
+            create_phi_vreg(
+                &mut lir_builder,
+                &mut value_reg_map,
+                backend,
+                valgraph,
+                output,
+            );
+        }
+
         for &node in schedule.scheduled_nodes_rev(block) {
+            // Values flowing into scheduled nodes start live, but may be made dead by the
+            // instruction selection process.
             for pred in dataflow_preds(valgraph, node) {
                 node_use_counts[pred] += 1;
             }
         }
     }
 
-    let mut lir_builder = LirBuilder::new(&cfg_ctx.block_order);
-
     while let Some(block) = lir_builder.advance_block() {
+        lir_builder.set_incoming_block_params(
+            schedule
+                .block_phis(block)
+                .iter()
+                .map(|&phi| value_reg_map[&valgraph.node_outputs(phi)[0]]),
+        );
+
+        for &succ in cfg_ctx.cfg.block_succs(block).iter() {
+            // TODO: Avoid linear search to deal with pathological cases?
+            let pred_idx = cfg_ctx
+                .cfg
+                .block_preds(succ)
+                .iter()
+                .position(|&pred| pred == block)
+                .unwrap();
+
+            let Some(valgraph_pred_idx) = cfg_ctx.block_map.valgraph_pred_index(succ, pred_idx)
+            else {
+                // This successor didn't come from the valgraph (it was a split critical edge).
+                continue;
+            };
+
+            let outgoing_params: SmallVec<[_; 4]> = schedule
+                .block_phis(succ)
+                .iter()
+                .map(|&phi| {
+                    let input = valgraph.node_inputs(phi)[valgraph_pred_idx as usize];
+                    get_value_vreg(
+                        &mut lir_builder,
+                        &mut value_reg_map,
+                        &mut reg_node_map,
+                        backend,
+                        valgraph,
+                        input,
+                    )
+                })
+                .collect();
+
+            lir_builder.add_succ_outgoing_block_params(outgoing_params)
+        }
+
         // TODO: Branch at end of block if necessary.
 
         for &node in schedule.scheduled_nodes_rev(block) {
@@ -178,6 +239,40 @@ pub fn select_instrs<B: Backend>(
     }
 
     Ok(lir_builder.finish())
+}
+
+fn get_value_vreg<B: Backend>(
+    lir_builder: &mut LirBuilder<'_, B::Instr>,
+    value_reg_map: &mut ValueRegMap,
+    reg_node_map: &mut RegNodeMap,
+    backend: &B,
+    valgraph: &ValGraph,
+    value: DepValue,
+) -> VirtReg {
+    get_value_vreg_helper(
+        |value, class| {
+            let vreg = lir_builder.create_vreg(class);
+            reg_node_map.insert(vreg, valgraph.value_def(value).0);
+            vreg
+        },
+        value_reg_map,
+        backend,
+        valgraph,
+        value,
+    )
+}
+
+fn create_phi_vreg<B: Backend>(
+    lir_builder: &mut LirBuilder<'_, B::Instr>,
+    value_reg_map: &mut ValueRegMap,
+    backend: &B,
+    valgraph: &ValGraph,
+    value: DepValue,
+) -> VirtReg {
+    let class = backend.reg_class_for_type(valgraph.value_kind(value).as_value().unwrap());
+    let vreg = lir_builder.create_vreg(class);
+    value_reg_map.insert(value, vreg);
+    vreg
 }
 
 fn get_value_vreg_helper<B: Backend>(

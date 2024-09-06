729187d87f0d8349f48d101df433fe86ae29c1b8
Noam Raz <noamraz8@gmail.com>
2024-09-07 02:17:35 +0300

    Build and run C API tests in parallel

--- c-api-tests/run.sh
+++ c-api-tests/run.sh
@@ -7,5 +7,5 @@ export CTEST_OUTPUT_ON_FAILURE=1
 
 mkdir -p build &&
     cmake -S . -B build &&
-    cmake --build build &&
-    cmake --build build --target test
+    cmake --build build -j$(nproc) &&
+    cmake --build build --target test -j$(nproc)

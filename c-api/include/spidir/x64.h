3b14b4ff768bf823f7b13ece567988adcf2dc07a
Noam Raz <noamraz8@gmail.com>
2024-09-07 22:26:12 +0300

    Declare x64 ABS64 relocation kind in C header

--- c-api/include/spidir/x64.h
+++ c-api/include/spidir/x64.h
@@ -12,6 +12,9 @@ enum {
     ///   relocation
     /// * `A` is the addend value stored in the relocation
     SPIDIR_RELOC_X64_PC32 = 0,
+    /// A 64-bit relocation whose value should be the full run-time address of
+    /// the function referred to by the relocation.
+    SPIDIR_RELOC_X64_ABS64 = 1,
 };
 
 /// Creates a machine backend that generates code for the x64 architecture.

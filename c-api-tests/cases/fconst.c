ce8b82de5ef835970c1e53f130e8fb4f2cda6d4d
Noam Raz <noamraz8@gmail.com>
2025-05-10 16:45:08 +0300

    Add `fconst` C API test
    Now that the ABI has been fixed, we can actually pass floating-point
    values through the C API.

---
+++ c-api-tests/cases/fconst.c
@@ -0,0 +1,22 @@
+#include <spidir/module.h>
+
+#include "utils.h"
+
+void builder_callback(spidir_builder_handle_t builder, void* ctx) {
+    (void) ctx;
+    spidir_block_t block = spidir_builder_create_block(builder);
+    spidir_builder_set_entry_block(builder, block);
+    spidir_builder_set_block(builder, block);
+    spidir_value_t retval = spidir_builder_build_fconst64(builder, 64.0);
+    spidir_builder_build_return(builder, retval);
+}
+
+int main(void) {
+    spidir_module_handle_t module = spidir_module_create();
+    spidir_function_t func =
+        spidir_module_create_function(module, "func", SPIDIR_TYPE_F64, 0, NULL);
+    spidir_module_build_function(module, func, builder_callback, NULL);
+    dump_module_to_stdout(module);
+    spidir_module_destroy(module);
+    return 0;
+}

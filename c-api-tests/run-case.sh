97942408c060caf32e536fd61bd3fcd5079414b2
Noam Raz <noamraz8@gmail.com>
2025-07-05 20:06:08 +0300

    Improve C API test scripts

--- c-api-tests/run-case.sh
+++ c-api-tests/run-case.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+set -euo pipefail
+
 EXPECTED_OUTPUT="$1"
 EXE_NAME="$2"
 
 TEMPFILE=$(mktemp)
-"$EXE_NAME" > "$TEMPFILE" || exit 1
+"$EXE_NAME" > "$TEMPFILE"
+
+set +e
 DIFF_OUTPUT=$(diff --color=always -u "$EXPECTED_OUTPUT" "$TEMPFILE")
-if [ $? -ne 0 ]; then
+DIFF_RETVAL=$?
+set -e
+
+if [ $DIFF_RETVAL -ne 0 ]; then
     if [ -z ${UPDATE_EXPECT+x} ]; then
         echo "$DIFF_OUTPUT"
         exit 1
--- c-api-tests/run.sh
+++ c-api-tests/run.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+set -euo pipefail
+
 cd $(dirname $(readlink -f "$0"))
 
 export CMAKE_EXPORT_COMPILE_COMMANDS=ON
 export CTEST_OUTPUT_ON_FAILURE=1
 
-mkdir -p build &&
-    cmake -S . -B build &&
-    cmake --build build -j$(nproc) &&
-    cmake --build build --target test -j$(nproc)
+mkdir -p build
+cmake -S . -B build
+cmake --build build -j$(nproc)
+cmake --build build --target test -j$(nproc)

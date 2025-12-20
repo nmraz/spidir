1d870f2e85fb2c51d8bb207ca0630f872928b52a
Noam Raz <noamraz8@gmail.com>
2025-12-21 00:23:27 +0200

    Extract `match_iconst` utility out of canonicalizer
    This is also a fairly generic operation we'll want elsewhere.

--- crates/opt/src/utils.rs
+++ crates/opt/src/utils.rs
@@ -1,6 +1,20 @@
-use ir::{builder::BuilderExt, valgraph::DepValue};
+use ir::{
+    builder::BuilderExt,
+    node::NodeKind,
+    valgraph::{DepValue, ValGraph},
+};
+use valmatch::match_value;
 
 use crate::state::EditContext;
+
+pub fn match_iconst(graph: &ValGraph, value: DepValue) -> Option<u64> {
+    match_value! {
+        if let &NodeKind::Iconst(val) = graph, value {
+            return Some(val);
+        }
+    }
+    None
+}
 
 pub fn replace_with_iconst(ctx: &mut EditContext<'_>, value: DepValue, iconst: u64) {
     let ty = ctx.graph().value_kind(value).as_value().unwrap();

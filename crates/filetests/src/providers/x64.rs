3d112a6da173bf35b15a4161ef35bc711e745848
Noam Raz <noamraz8@gmail.com>
2026-06-19 15:48:09 +0300

    Change how code models are passed to x64 filetests
    Differing internal/external code models are now passed as a single
    parenthesized parameter to the isel/codegen providers. Doing things this
    way will make it easier to pass CPU features soon.

--- crates/filetests/src/providers/x64.rs
+++ crates/filetests/src/providers/x64.rs
@@ -1,22 +1,37 @@
-use anyhow::{Result, bail};
+use anyhow::{Ok, Result, bail, ensure};
 use codegen::target::x64::{CodeModel, X64Machine, X64MachineConfig};
 
 pub fn create_x64_machine(params: &[&str]) -> Result<X64Machine> {
     match params {
         [] => Ok(X64Machine::default()),
-        [code_model] => {
-            let code_model = parse_code_model(code_model)?;
+        [code_models] => {
+            let (internal_code_model, extern_code_model) = parse_code_models(code_models)?;
             Ok(X64Machine::new(X64MachineConfig {
-                internal_code_model: code_model,
-                extern_code_model: code_model,
+                internal_code_model,
+                extern_code_model,
             }))
         }
-        [internal_code_model, extern_code_model] => Ok(X64Machine::new(X64MachineConfig {
-            internal_code_model: parse_code_model(internal_code_model)?,
-            extern_code_model: parse_code_model(extern_code_model)?,
-        })),
         _ => bail!("invalid parameter count for x64 backend provider"),
     }
+}
+
+fn parse_code_models(code_models: &str) -> Result<(CodeModel, CodeModel)> {
+    if code_models.starts_with('(') {
+        ensure!(code_models.ends_with(')'), "invalid code model description");
+        let code_models = code_models[1..code_models.len() - 1]
+            .split(',')
+            .collect::<Vec<_>>();
+        let &[internal_code_model, extern_code_model] = &code_models[..] else {
+            bail!("expected two code models in parenthesized description")
+        };
+        return Ok((
+            parse_code_model(internal_code_model.trim())?,
+            parse_code_model(extern_code_model.trim())?,
+        ));
+    }
+
+    let code_model = parse_code_model(code_models)?;
+    Ok((code_model, code_model))
 }
 
 fn parse_code_model(code_model: &str) -> Result<CodeModel> {
